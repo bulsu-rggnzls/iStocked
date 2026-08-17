c12f08989d6a535825b1a45071b08dd271d99fe9
bulsu-rggnzls <argiegonzales463@gmail.com>
2026-08-17 21:47:01 +0800

    feat: add root layout with expo-router stack

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import "../global.css";
+
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "iStocked" }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
+  );
+}
