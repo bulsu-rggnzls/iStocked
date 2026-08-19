eef0339f6cfb853fab5d7ee64eb9dcab7302eb9e
bulsu-rggnzls <argiegonzales463@gmail.com>
2026-08-19 08:32:53 +0800

    fix: serve COEP/COOP headers for sqlite web

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,12 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push("wasm");
 
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    return middleware(req, res, next);
+  };
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
